ab70207d240ea1cb685a580688094ad81d0cbf85
AnatoliySereda <as0996497@RK03-B1225-08.francistuttle.edu>
2018-01-26 15:41:40 -0600

    sdf

--- src/Chapter7/C7_26.java
+++ src/Chapter7/C7_26.java
@@ -1,5 +1,5 @@
-
 package Chapter7;
+
 import java.util.Scanner;
 
 /**
@@ -9,52 +9,55 @@ import java.util.Scanner;
  * @author Anatoliy Sereda
  */
 public class C7_26 {
-   
+
     /**
      * Main Method
      *
      * @param args arguments from command line prompt
      */
-
     public static void main(String[] args) {
-  Scanner input = new Scanner(System.in);
- 
-  System.out.print("Enter list1:");
-  int[] list1 = new int[input.nextInt()];
-  for (int i = 0; i < list1.length; i++) {
-   list1[i] = input.nextInt();
-  }
- 
-  System.out.print("Enter list2:");
-  int[] list2 = new int[input.nextInt()];
-  for (int i = 0; i < list2.length; i++) {
-   list2[i] = input.nextInt();
-  }
- 
-  if (equals(list1, list2)) {
-   System.out.println("Two lists are strictly identical");
-  } else {
-   System.out.println("Two lists are not strictly identical");
-  }
- 
- }
- /**
-  * check whether the amount of letters is the same
-  * @param list1 comparison
-  * @param list2 comparison
-  * @return whether a string is identical
-  */
- public static boolean equals(int[] list1, int[] list2) {
- 
-  if (list1.length != list2.length)
-   return false;
- 
-  for (int i = 0; i < list2.length; i++) {
-   if (list1[i] != list2[i])
-    return false;
-  }
- 
-  return true;
- 
- }
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter list1:");
+        int[] list1 = new int[input.nextInt()];
+        for (int i = 0; i < list1.length; i++) {
+            list1[i] = input.nextInt();
+        }
+
+        System.out.print("Enter list2:");
+        int[] list2 = new int[input.nextInt()];
+        for (int i = 0; i < list2.length; i++) {
+            list2[i] = input.nextInt();
+        }
+
+        if (equals(list1, list2)) {
+            System.out.println("Two lists are strictly identical");
+        } else {
+            System.out.println("Two lists are not strictly identical");
+        }
+
+    }
+
+    /**
+     * check whether the amount of letters is the same
+     *
+     * @param list1 comparison
+     * @param list2 comparison
+     * @return whether a string is identical
+     */
+    public static boolean equals(int[] list1, int[] list2) {
+
+        if (list1.length != list2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < list2.length; i++) {
+            if (list1[i] != list2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
 }
--- src/Chapter7/C7_9.java
+++ src/Chapter7/C7_9.java
@@ -15,36 +15,36 @@ public class C7_9 {
      *
      * @param args arguments from command line prompt
      */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.print("Enter ten numbers:");
+        Scanner input = new Scanner(System.in);
+        double[] numbers = new double[10];
 
-     public static void main(String[] args) {
-  // TODO Auto-generated method stub
-  System.out.print("Enter ten numbers:");
-  Scanner input = new Scanner(System.in);
-  double[] numbers = new double[10];
- 
-  for (int i = 0; i < numbers.length; i++) {
-   numbers[i] = input.nextDouble();
-  }
- 
-  System.out.println("The minimum number is: " + min(numbers));
- }
- /**
-  * It makes the minimum amount 8 for a password
-  * @param array this is the 10 numbers
-  * @return the lowest array
-  */
- public static double min(double[] array) {
- 
-  double min = array[0];
- 
-  for (int i = 1; i < array.length; i++) {
-   if (min > array[i]) {
-    min = array[i];
-   }
-  }
- 
-  return min;
- 
- }
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = input.nextDouble();
+        }
+
+        System.out.println("The minimum number is: " + min(numbers));
+    }
+
+    /**
+     * It makes the minimum amount 8 for a password
+     *
+     * @param array this is the 10 numbers
+     * @return the lowest array
+     */
+    public static double min(double[] array) {
+
+        double min = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+
+        return min;
+
+    }
 }
-
